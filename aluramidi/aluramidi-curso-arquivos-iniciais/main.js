64d8934fa47310fb32cb8fefc3306ffc20beab92
Gabriela Mattos <gabrielamattosde@gmail.com>
2022-05-12 10:16:15 -0300

    Alterando o laço de repetição

--- aluramidi/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi/aluramidi-curso-arquivos-iniciais/main.js
@@ -7,29 +7,27 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
 
-// o enquanto espera receber uma informação V ou F
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
 
     const instrumento = tecla.classList[1];
 
-    // tamplate string
-    const idAudio = `#som_${instrumento}`;
-    //console.log(idAudio);
+    const idAudio = `#som_${instrumento}`; //tamplate string
 
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    contador = contador + 1;
-
-    //console.log(contador);
-
 
 }
+
+
+
+// Forma de chamar um input
+//const inputTel = document.querySelector('input[type=tel]');
+
 
